f63b448f7aa044c4909cd15aaaba858902ece62a
Stanko Krtalić <stanko.krtalic@gmail.com>
2017-03-02 10:37:35 +0100

    Set root to landing

--- configurator/config/routes.rb
+++ configurator/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'template#edit'
+  root to: 'landing#show'
   resource :type, only: [:edit, :update], controller: 'type'
   resource :template, only: [:edit, :update], controller: 'template' do
     get 'data'
