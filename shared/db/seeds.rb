25fc61604f6629e3a6093264ca1b6d5de9da7074
Stanko Krtalić <stanko.krtalic@gmail.com>
2017-03-02 10:51:38 +0100

    Set API Gateway Proxy as default template

--- shared/db/seeds.rb
+++ shared/db/seeds.rb
@@ -1,3 +1,17 @@
-Setting.create(key: 'type', value: 'Vanilla')
-Setting.create(key: 'template', value: '{}')
+# Set default type
+type = Setting.find_by(key: 'type')
+if !type
+  Setting.create(key: 'type', value: 'Vanilla')
+end
+
+# Set default template
+template = Setting.find_by(key: 'template')
+if !template
+  name = 'API Gateway AWS Proxy'
+  template = TemplateLoader.call.select do |template|
+    template.name == name
+  end.first
+
+  Setting.create(key: 'template', value: template.data.to_json)
+end
 
